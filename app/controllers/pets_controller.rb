bc30ac0c59777ac7c5c32c36b30a7656bec057f8
Ariel I <arielibarra39@gmail.com>
2020-10-30 21:18:01 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -21,7 +21,6 @@ class PetsController < ApplicationController
     redirect to "pets/#{@pet.id}"
   end
 
-
   get '/pets/:id' do 	 
     @pet = Pet.find(params[:id])
     @owner = Owner.all
@@ -34,20 +33,14 @@ class PetsController < ApplicationController
       erb :'/pets/edit'
   end	
 
-
   patch '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
-
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-
     @pet.save
-
     redirect to "pets/#{@pet.id}"
   end
-    
-    
-   
+  
 end
